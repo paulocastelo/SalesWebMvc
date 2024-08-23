fb12c36db78d663592d7aad2ea7675beab9ebfc0
Paulo Castelo <oluap.castelo@gmail.com>
2024-08-23 19:49:13 -0300

    Simple create form.

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,5 +21,10 @@ namespace SalesWebMvc.Services
             // Sicrona
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
